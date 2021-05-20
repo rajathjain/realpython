49994415e2709d959bb146048fa39cdcf5259a0b
rajathjain <rajathjain03@gmail.com>
2021-05-20 21:29:25 +0530

    Basic_Threads

---
+++ Threads/joining_threads.py
@@ -0,0 +1,17 @@
+import time
+import threading
+
+def myfunc(name):
+    print(f'my func started with {name}')
+    time.sleep(10)
+    print('my func ended')
+    return
+
+if __name__ == '__main__':
+    print('main started')
+    t = threading.Thread(target=myfunc, args=['Real Python'])
+    #main thread creating a new thread
+    t.start()
+    t.join()
+    #This statements gets printed before the function call return
+    print('main ended')
---
+++ Threads/thread.py
@@ -0,0 +1,16 @@
+import time
+import threading
+
+def myfunc(name):
+    print(f'my func started with {name}')
+    time.sleep(10)
+    print('my func ended')
+    return
+
+if __name__ == '__main__':
+    print('main started')
+    t = threading.Thread(target=myfunc, args=['Real Python'])
+    #main thread creating a new thread
+    t.start()
+    #This statements gets printed before the function call return
+    print('main ended')
